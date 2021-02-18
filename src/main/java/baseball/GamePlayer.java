da207b8f8aaef311a8ebeac792c67a21932560cf
Chae <eqholic1125@gmail.com>
2021-02-18 14:32:10 +0900

    feat(*): 비정상적인 입력 처리 기능 추가

--- src/main/java/baseball/GamePlayer.java
+++ src/main/java/baseball/GamePlayer.java
@@ -3,19 +3,28 @@ package baseball;
 
 import java.util.Scanner;
 
+import static baseball.Number.*;
+
 public class GamePlayer {
 
     public static void play(Scanner Input){
         Number random = new Number();
         Number player = new Number();
+        Integer temp;
 
+        //랜덤 수 생성
         random.setNumber(random.createNumber());
 
-            System.out.print("숫자를 입력해 주세요 : ");
-            player.setNumber(Number.splitNumber(Input.nextInt()));
-            System.out.println(player.getNumber());
+        //입력 받고 비정상적인 입력 처리
+        System.out.print("숫자를 입력해 주세요 : ");
+        temp = Input.nextInt();
+        inputCheck(temp);
+
+        //입력 수 저장
+        player.setNumber(splitNumber(temp));
 
     }
+
 
 
 }
