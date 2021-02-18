d252b821d21fcb2a596e68b63e7427f136f6581e
DongMin-Lee <pkalsh345@gmail.com>
2021-02-19 05:24:35 +0900

    test: 비교하여 결과 설정하는 모든 테스트 케이스 구현
    각 테스트 메서드 상황:
    - 노카운트 case
    - 1스트라이크 case
    - 1스트라이크 1볼 case
    - 1스트라이크 2볼 case
    - 2스트라이크 case
    - 3스트라이크 case

--- src/test/java/baseball/number/GameNumberTest.java
+++ src/test/java/baseball/number/GameNumberTest.java
@@ -1,5 +1,6 @@
 package baseball.number;
 
+import baseball.result.Result;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,7 +8,201 @@ import static org.junit.jupiter.api.Assertions.*;
 class GameNumberTest {
 
     @Test
-    void compare() {
+    void case_노카운트_검증() {
+        //given
+        final int TEST_CASE_NUM = 4;
+        GameNumber[] userNumbers = new GameNumber[TEST_CASE_NUM];
+        GameNumber[] seedNumbers = new GameNumber[TEST_CASE_NUM];
+        Result[] results = new Result[TEST_CASE_NUM];
+
+        userNumbers[0] = new GameNumber("123");
+        seedNumbers[0] = new GameNumber("456");
+        results[0] = new Result();
+
+        userNumbers[1] = new GameNumber("984");
+        seedNumbers[1] = new GameNumber("712");
+        results[1] = new Result();
+
+        userNumbers[2] = new GameNumber("921");
+        seedNumbers[2] = new GameNumber("743");
+        results[2] = new Result();
+
+        userNumbers[3] = new GameNumber("652");
+        seedNumbers[3] = new GameNumber("981");
+        results[3] = new Result();
+
+        for (int testCase = 0; testCase < TEST_CASE_NUM; testCase++) {
+            //when
+            seedNumbers[testCase].compare(userNumbers[testCase], results[testCase]);
+            //then
+            assertEquals(results[testCase].getBallCount(), 0);
+            assertEquals(results[testCase].getStrikeCount(), 0);
+        }
+    }
+
+    @Test
+    void case_1스트라이크_검증() {
+        //given
+        final int TEST_CASE_NUM = 4;
+        GameNumber[] userNumbers = new GameNumber[TEST_CASE_NUM];
+        GameNumber[] seedNumbers = new GameNumber[TEST_CASE_NUM];
+        Result[] results = new Result[TEST_CASE_NUM];
+
+        userNumbers[0] = new GameNumber("783");
+        seedNumbers[0] = new GameNumber("582");
+        results[0] = new Result();
+
+        userNumbers[1] = new GameNumber("987");
+        seedNumbers[1] = new GameNumber("237");
+        results[1] = new Result();
+
+        userNumbers[2] = new GameNumber("127");
+        seedNumbers[2] = new GameNumber("597");
+        results[2] = new Result();
+
+        userNumbers[3] = new GameNumber("386");
+        seedNumbers[3] = new GameNumber("746");
+        results[3] = new Result();
+
+        for (int testCase = 0; testCase < TEST_CASE_NUM; testCase++) {
+            //when
+            seedNumbers[testCase].compare(userNumbers[testCase], results[testCase]);
+            //then
+            assertEquals(results[testCase].getBallCount(), 0);
+            assertEquals(results[testCase].getStrikeCount(), 1);
+        }
+    }
+
+    @Test
+    void case_1스트라이크_1볼_검증() {
+        //given
+        final int TEST_CASE_NUM = 4;
+        GameNumber[] userNumbers = new GameNumber[TEST_CASE_NUM];
+        GameNumber[] seedNumbers = new GameNumber[TEST_CASE_NUM];
+        Result[] results = new Result[TEST_CASE_NUM];
+
+        userNumbers[0] = new GameNumber("783");
+        seedNumbers[0] = new GameNumber("287");
+        results[0] = new Result();
+
+        userNumbers[1] = new GameNumber("987");
+        seedNumbers[1] = new GameNumber("497");
+        results[1] = new Result();
+
+        userNumbers[2] = new GameNumber("127");
+        seedNumbers[2] = new GameNumber("917");
+        results[2] = new Result();
+
+        userNumbers[3] = new GameNumber("386");
+        seedNumbers[3] = new GameNumber("236");
+        results[3] = new Result();
+
+        for (int testCase = 0; testCase < TEST_CASE_NUM; testCase++) {
+            //when
+            seedNumbers[testCase].compare(userNumbers[testCase], results[testCase]);
+            //then
+            assertEquals(results[testCase].getBallCount(), 1);
+            assertEquals(results[testCase].getStrikeCount(), 1);
+        }
+    }
+
+    @Test
+    void case_1스트라이크_2볼_검증() {
+        //given
+        final int TEST_CASE_NUM = 4;
+        GameNumber[] userNumbers = new GameNumber[TEST_CASE_NUM];
+        GameNumber[] seedNumbers = new GameNumber[TEST_CASE_NUM];
+        Result[] results = new Result[TEST_CASE_NUM];
+
+        userNumbers[0] = new GameNumber("783");
+        seedNumbers[0] = new GameNumber("387");
+        results[0] = new Result();
+
+        userNumbers[1] = new GameNumber("987");
+        seedNumbers[1] = new GameNumber("897");
+        results[1] = new Result();
+
+        userNumbers[2] = new GameNumber("127");
+        seedNumbers[2] = new GameNumber("217");
+        results[2] = new Result();
+
+        userNumbers[3] = new GameNumber("386");
+        seedNumbers[3] = new GameNumber("836");
+        results[3] = new Result();
+
+        for (int testCase = 0; testCase < TEST_CASE_NUM; testCase++) {
+            //when
+            seedNumbers[testCase].compare(userNumbers[testCase], results[testCase]);
+            //then
+            assertEquals(results[testCase].getBallCount(), 2);
+            assertEquals(results[testCase].getStrikeCount(), 1);
+        }
+    }
+
+    @Test
+    void case_2스트라이크_검증() {
+        //given
+        final int TEST_CASE_NUM = 4;
+        GameNumber[] userNumbers = new GameNumber[TEST_CASE_NUM];
+        GameNumber[] seedNumbers = new GameNumber[TEST_CASE_NUM];
+        Result[] results = new Result[TEST_CASE_NUM];
+
+        userNumbers[0] = new GameNumber("783");
+        seedNumbers[0] = new GameNumber("583");
+        results[0] = new Result();
+
+        userNumbers[1] = new GameNumber("987");
+        seedNumbers[1] = new GameNumber("287");
+        results[1] = new Result();
+
+        userNumbers[2] = new GameNumber("127");
+        seedNumbers[2] = new GameNumber("137");
+        results[2] = new Result();
+
+        userNumbers[3] = new GameNumber("386");
+        seedNumbers[3] = new GameNumber("382");
+        results[3] = new Result();
+
+        for (int testCase = 0; testCase < TEST_CASE_NUM; testCase++) {
+            //when
+            seedNumbers[testCase].compare(userNumbers[testCase], results[testCase]);
+            //then
+            assertEquals(results[testCase].getBallCount(), 0);
+            assertEquals(results[testCase].getStrikeCount(), 2);
+        }
+    }
+
+    @Test
+    void case_3스트라이크_검증() {
+        //given
+        final int TEST_CASE_NUM = 4;
+        GameNumber[] userNumbers = new GameNumber[TEST_CASE_NUM];
+        GameNumber[] seedNumbers = new GameNumber[TEST_CASE_NUM];
+        Result[] results = new Result[TEST_CASE_NUM];
+
+        userNumbers[0] = new GameNumber("783");
+        seedNumbers[0] = new GameNumber("783");
+        results[0] = new Result();
+
+        userNumbers[1] = new GameNumber("987");
+        seedNumbers[1] = new GameNumber("987");
+        results[1] = new Result();
+
+        userNumbers[2] = new GameNumber("127");
+        seedNumbers[2] = new GameNumber("127");
+        results[2] = new Result();
+
+        userNumbers[3] = new GameNumber("386");
+        seedNumbers[3] = new GameNumber("386");
+        results[3] = new Result();
+
+        for (int testCase = 0; testCase < TEST_CASE_NUM; testCase++) {
+            //when
+            seedNumbers[testCase].compare(userNumbers[testCase], results[testCase]);
+            //then
+            assertEquals(results[testCase].getBallCount(), 0);
+            assertEquals(results[testCase].getStrikeCount(), 3);
+        }
     }
 
     @Test
